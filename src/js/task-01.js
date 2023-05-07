8536e55d1917fc775372b63c9b8a71f3fc8139d4
MarDak24 <maxim.matjerow@gmail.com>
2023-05-07 17:35:14 +0300

    Task 1 && Task 2

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = document.querySelectorAll(".item");
+
+//  POINT 1
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+// POINT 2
+categoriesItems.forEach((category) => {
+  const elementTitleText = category.querySelector("h2").textContent;
+  const totalNumberOfElements = category.querySelectorAll("li").length;
+
+  console.log(`Category: ${elementTitleText}`);
+  console.log(`Elements: ${totalNumberOfElements}`);
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsItem = ingredients.map((ingredient) => {
+  // POINT 1
+  const item = document.createElement("li");
+  // POINT 2
+  item.textContent = ingredient;
+  // POINT 3
+  item.classList.add("item");
+
+  return item;
+});
+// POINT 4
+ingredientsList.append(...ingredientsItem);
